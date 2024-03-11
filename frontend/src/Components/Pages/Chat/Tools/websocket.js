fbdf6fad498e9a092ac58cf037e1cc831f41340e
dave502 <davtyan.d.n@mail.ru>
2024-03-11 11:04:49 +0200

    fixed translation

--- frontend/src/Components/Pages/Chat/Tools/websocket.js
+++ frontend/src/Components/Pages/Chat/Tools/websocket.js
@@ -2,8 +2,8 @@
 let websocket = (newMessage, jwt) => {
   console.log("connecting")
   const host = window.location.hostname;
-  console.log("const host", host)
-  var socket = new WebSocket("ws://" + host + "/chat/v1/ws");
+  console.log("const host", "wss://" + host + "/wschat/v1/ws")
+  var socket = new WebSocket("wss://" + host + "/wschat/v1/ws");
 
   socket.onopen = () => {
     console.log("Successfully Connected");
--- frontend/src/Components/Pages/Friends/Components/RecommendedList.jsx
+++ frontend/src/Components/Pages/Friends/Components/RecommendedList.jsx
@@ -1,9 +1,8 @@
 import { VStack, Text } from "@chakra-ui/react";
 import UserListItem from "./UserListItem";
 import { gql, useQuery, useReactiveVar } from "@apollo/client";
-import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
-import { useEffect, useState } from 'react';
-
+import { SkeletonCircle, SkeletonText } from '@chakra-ui/react'
+import { useTranslation } from "react-i18next";
 
 const GET_SIMILAR_USERS_QUERY = gql`
   query similarUsers($uid:String!){
@@ -33,34 +32,36 @@ function RecommendedList(props){
     
   const { uid } = props; 
   
-    const { loading, error, data } = useQuery(GET_SIMILAR_USERS_QUERY, {
+  const { t } = useTranslation();
+  
+  const { loading, error, data } = useQuery(GET_SIMILAR_USERS_QUERY, {
       variables: { uid: uid },
     });
     
-    if (loading) return (
-      <VStack>
-        <SkeletonCircle size='10' />
-        <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-      </VStack>
-    );
-    if (error) return `Error! ${error}`;
-    
-   return (
+  if (loading) return (
     <VStack>
-    {data? (
-        <>
-        {data.similarUsers.length ? data.similarUsers.map((obj) => (
-                <UserListItem currentUid={uid} user={obj} mode="recomendations" key={obj.uid}/>
-            )):(
-               <Text>Рекомендации не найдены.</Text> 
-            )
-        }
-        </>
-    ):
-    <Text>Поиск...</Text>
-    }
+      <SkeletonCircle size='10' />
+      <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
     </VStack>
-   );
- }
+  );
+  if (error) return `Error! ${error}`;
+    
+  return (
+  <VStack>
+  {data? (
+      <>
+      {data.similarUsers.length ? data.similarUsers.map((obj) => (
+              <UserListItem currentUid={uid} user={obj} mode="recomendations" key={obj.uid}/>
+          )):(
+              <Text>{t("no_recommendations")}</Text> 
+          )
+      }
+      </>
+  ):
+  <Text>Поиск...</Text>
+  }
+  </VStack>
+  );
+}
  
  export default RecommendedList;
