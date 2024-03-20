b585eefb45c736157bc814c4a8721574b2e236f0
dave502 <davtyan.d.n@mail.ru>
2024-03-20 16:04:49 +0200

    avatar updates

--- frontend/src/Components/Pages/Profile/Components/ProfileForm.jsx
+++ frontend/src/Components/Pages/Profile/Components/ProfileForm.jsx
@@ -147,7 +147,7 @@ function ProfileForm(props) {
                 <FormControl isInvalid={form.errors.name && form.touched.name} mb={3}>
                   <HStack>
                     <FormLabel style={{"width":'100px'}}>{t("photo")}</FormLabel>
-                    <UploadAvatar setFieldValue={setFieldValue} field={field} blobImg={values.blobImg}/>
+                    <UploadAvatar setFieldValue={setFieldValue} field={field} blobImg={values.img}/>
                   </HStack>
                   <FormErrorMessage>"{form.errors.name}"</FormErrorMessage>
                 </FormControl>
